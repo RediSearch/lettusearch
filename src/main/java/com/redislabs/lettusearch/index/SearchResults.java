f9047a5aa876ae759961c50558c6edf866b48515
jruaux <jruaux@jrx.home.ruaux.org>
2018-09-09 21:59:25 -0700

    removed @Data for marker classes

--- src/main/java/com/redislabs/lettusearch/index/SearchResults.java
+++ src/main/java/com/redislabs/lettusearch/index/SearchResults.java
@@ -1,10 +1,5 @@
 package com.redislabs.lettusearch.index;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
 public class SearchResults<K, V> extends AbstractSearchResults<K, V> {
 
 }
