11c49192018a9978629ebad82b8a9095eb7a6ff7
Julien Ruaux <jruaux@gmail.com>
2019-02-21 00:37:21 -0800

    added unit tests for search and aggregation

--- src/test/java/com/redislabs/lettusearch/AggregateTest.java
+++ src/test/java/com/redislabs/lettusearch/AggregateTest.java
@@ -2,20 +2,8 @@ package com.redislabs.lettusearch;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.redislabs.lettusearch.aggregate.AggregateOptions;
 import com.redislabs.lettusearch.aggregate.AggregateResults;
 import com.redislabs.lettusearch.aggregate.Group;
@@ -24,72 +12,8 @@ import com.redislabs.lettusearch.aggregate.Reduce;
 import com.redislabs.lettusearch.aggregate.Sort;
 import com.redislabs.lettusearch.aggregate.SortProperty;
 import com.redislabs.lettusearch.aggregate.reduce.Avg;
-import com.redislabs.lettusearch.search.AddOptions;
-import com.redislabs.lettusearch.search.Schema;
-import com.redislabs.lettusearch.search.Schema.SchemaBuilder;
-import com.redislabs.lettusearch.search.field.NumericField;
-import com.redislabs.lettusearch.search.field.TextField;
 
-public class AggregateTest {
-
-//	private final static String FIELD_INDEX = "index";
-	private final static String FIELD_ABV = "abv";
-//	private final static String FIELD_IBU = "ibu";
-	private final static String FIELD_ID = "id";
-	private final static String FIELD_NAME = "name";
-	private final static String FIELD_STYLE = "style";
-//	private final static String FIELD_BREWERY_ID = "brewery_id";
-	private final static String FIELD_OUNCES = "ounces";
-
-	private final static String INDEX = "beers";
-
-	private RediSearchClient client;
-	private StatefulRediSearchConnection<String, String> connection;
-	private List<Map<String, String>> beers;
-
-	@Before
-	public void setup() throws IOException {
-		this.beers = loadBeers("beers.csv");
-		client = RediSearchClient.create("redis://localhost");
-		connection = client.connect();
-		RediSearchCommands<String, String> commands = connection.sync();
-		commands.flushall();
-		SchemaBuilder schema = Schema.builder();
-		schema.field(TextField.builder().name(FIELD_NAME).sortable(true).build());
-		schema.field(TextField.builder().name(FIELD_STYLE).sortable(true).build());
-		schema.field(NumericField.builder().name(FIELD_ABV).sortable(true).build());
-		schema.field(NumericField.builder().name(FIELD_OUNCES).sortable(true).build());
-		commands.create(INDEX, schema.build());
-		for (Map<String, String> beer : beers) {
-			Map<String, String> cleanBeer = new HashMap<>();
-			for (Entry<String, String> entry : beer.entrySet()) {
-				if (entry.getValue() == null) {
-					continue;
-				}
-				cleanBeer.put(entry.getKey(), entry.getValue());
-			}
-			commands.add(INDEX, cleanBeer.get(FIELD_ID), 1, cleanBeer, AddOptions.builder().build());
-		}
-	}
-
-	private List<Map<String, String>> loadBeers(String fileName) throws IOException {
-		CsvSchema schema = CsvSchema.builder().setUseHeader(true).setNullValue("").build();
-		CsvMapper mapper = new CsvMapper();
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
-		MappingIterator<Map<String, String>> readValues = mapper.readerFor(Map.class).with(schema)
-				.readValues(inputStream);
-		return readValues.readAll();
-	}
-
-	@After
-	public void teardown() {
-		if (connection != null) {
-			connection.close();
-		}
-		if (client != null) {
-			client.shutdown();
-		}
-	}
+public class AggregateTest extends BaseTest {
 
 	@Test
 	public void testAggregateLoad() {
--- src/test/java/com/redislabs/lettusearch/SearchTest.java
+++ src/test/java/com/redislabs/lettusearch/SearchTest.java
@@ -1,126 +1,47 @@
 package com.redislabs.lettusearch;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.redislabs.lettusearch.search.AddOptions;
-import com.redislabs.lettusearch.search.Schema;
+import com.redislabs.lettusearch.search.Limit;
 import com.redislabs.lettusearch.search.SearchOptions;
 import com.redislabs.lettusearch.search.SearchResults;
-import com.redislabs.lettusearch.search.api.sync.SearchCommands;
-import com.redislabs.lettusearch.search.field.TextField;
-import com.redislabs.lettusearch.search.field.TextField.Matcher;
-import com.redislabs.lettusearch.suggest.SuggestAddOptions;
-import com.redislabs.lettusearch.suggest.SuggestGetOptions;
-import com.redislabs.lettusearch.suggest.SuggestResult;
-import com.redislabs.lettusearch.suggest.api.sync.SuggestCommands;
 
-public class SearchTest {
-
-	private final static String INDEX = "testIndex";
-	private static final String FIELD1 = "field1";
-	private static final String FIELD2 = "field2";
-	private static final String DOC1 = "doc1";
-	private static final String DOC2 = "doc2";
-	private RediSearchClient client;
-
-	@Before
-	public void setup() {
-		client = RediSearchClient.create("redis://localhost");
-		client.connect().sync().flushall();
-	}
-
-	@After
-	public void teardown() {
-		client.shutdown();
-	}
-
-	@Test
-	public void testAdd() {
-		StatefulRediSearchConnection<String, String> connection = client.connect();
-		SearchCommands<String, String> commands = connection.sync();
-		connection.sync().create(INDEX, Schema.builder().field(TextField.builder().name(FIELD1).build())
-				.field(TextField.builder().name(FIELD2).sortable(true).build()).build());
-		Map<String, String> fields1 = new HashMap<>();
-		fields1.put(FIELD1, "this is doc 1 value 1");
-		fields1.put(FIELD2, "this is doc 1 value 2");
-		Map<String, String> fields2 = new HashMap<>();
-		fields2.put(FIELD1, "this is doc 2 value 1");
-		fields2.put(FIELD2, "this is doc 2 value 2");
-		commands.add(INDEX, DOC1, 1, fields1, AddOptions.builder().build());
-		commands.add(INDEX, DOC2, 1, fields2, AddOptions.builder().build());
-		connection.close();
-	}
+public class SearchTest extends BaseTest {
 
 	@Test
 	public void testPhoneticFields() {
-		String index = "traps";
-		StatefulRediSearchConnection<String, String> connection = client.connect();
-		SearchCommands<String, String> commands = connection.sync();
-		commands.create(index,
-				Schema.builder().field(TextField.builder().name("word").matcher(Matcher.English).build()).build());
-		Map<String, String> fields1 = new HashMap<>();
-		fields1.put("word", "kar");
-		Map<String, String> fields2 = new HashMap<>();
-		fields2.put("word", "car");
-		commands.add(index, DOC1, 1, fields1, AddOptions.builder().build());
-		commands.add(index, DOC2, 1, fields2, AddOptions.builder().build());
-		SearchResults<String, String> results = commands.search(index, "qar",
-				SearchOptions.builder().withScores(true).build());
-		Assert.assertEquals(2, results.getCount());
-		connection.close();
+		SearchResults<String, String> results = connection.sync().search(INDEX, "@style:pail",
+				SearchOptions.builder().build());
+		Assert.assertEquals(445, results.getCount());
 	}
 
-	@Test
-	public void testSuggestions() {
-		String key = "artists";
-		StatefulRediSearchConnection<String, String> connection = client.connect();
-		SuggestCommands<String, String> commands = connection.sync();
-		String hancock = "Herbie Hancock";
-		String mann = "Herbie Mann";
-		commands.sugadd(key, hancock, 1, SuggestAddOptions.builder().build());
-		commands.sugadd(key, mann, 1, SuggestAddOptions.builder().build());
-		commands.sugadd(key, "DJ Herbie", 1, SuggestAddOptions.builder().build());
-		List<SuggestResult<String>> results = commands.sugget(key, "Herb",
-				SuggestGetOptions.builder().withScores(true).withPayloads(true).build());
-		Assert.assertEquals(2, results.size());
-		Assert.assertTrue(results.stream().anyMatch(result -> hancock.equals(result.getString())));
-		Assert.assertTrue(results.stream().anyMatch(result -> mann.equals(result.getString())));
-	}
-
-	@Test
-	public void testSearch() {
-		testAdd();
-		StatefulRediSearchConnection<String, String> connection = client.connect();
-		SearchResults<String, String> results = connection.sync().search(INDEX, "value",
-				SearchOptions.builder().withScores(true).build());
-		Assert.assertEquals(2, results.getCount());
-		Assert.assertEquals(2, results.getResults().size());
-		Assert.assertEquals(DOC2, results.getResults().get(0).getDocumentId());
-		Assert.assertEquals(DOC1, results.getResults().get(1).getDocumentId());
-		Assert.assertEquals("this is doc 1 value 1", results.getResults().get(1).getFields().get(FIELD1));
-		Assert.assertEquals("this is doc 2 value 2", results.getResults().get(0).getFields().get(FIELD2));
-		Assert.assertEquals(0.1333333, results.getResults().get(0).getScore(), 0.000001);
-		connection.close();
-	}
-
+//	@Test
+//	public void testSuggestions() {
+//		String key = "artists";
+//		StatefulRediSearchConnection<String, String> connection = client.connect();
+//		SuggestCommands<String, String> commands = connection.sync();
+//		String hancock = "Herbie Hancock";
+//		String mann = "Herbie Mann";
+//		commands.sugadd(key, hancock, 1, SuggestAddOptions.builder().build());
+//		commands.sugadd(key, mann, 1, SuggestAddOptions.builder().build());
+//		commands.sugadd(key, "DJ Herbie", 1, SuggestAddOptions.builder().build());
+//		List<SuggestResult<String>> results = commands.sugget(key, "Herb",
+//				SuggestGetOptions.builder().withScores(true).withPayloads(true).build());
+//		Assert.assertEquals(2, results.size());
+//		Assert.assertTrue(results.stream().anyMatch(result -> hancock.equals(result.getString())));
+//		Assert.assertTrue(results.stream().anyMatch(result -> mann.equals(result.getString())));
+//	}
+//
+//
 	@Test
 	public void testSearchNoContent() {
-		testAdd();
-		StatefulRediSearchConnection<String, String> connection = client.connect();
-		SearchResults<String, String> results = connection.sync().search(INDEX, "value",
-				SearchOptions.builder().withScores(true).noContent(true).build());
-		Assert.assertEquals(2, results.getCount());
-		Assert.assertEquals(2, results.getResults().size());
-		Assert.assertEquals(DOC2, results.getResults().get(0).getDocumentId());
-		Assert.assertEquals(DOC1, results.getResults().get(1).getDocumentId());
-		Assert.assertEquals(0.1333333, results.getResults().get(0).getScore(), 0.000001);
+		SearchResults<String, String> results = connection.sync().search(INDEX, "Hefeweizen", SearchOptions.builder()
+				.withScores(true).noContent(true).limit(Limit.builder().num(100).build()).build());
+		Assert.assertEquals(42, results.getCount());
+		Assert.assertEquals(42, results.getResults().size());
+		Assert.assertEquals("1836", results.getResults().get(0).getDocumentId());
+		Assert.assertEquals(7.5, results.getResults().get(0).getScore(), 0.000001);
 	}
 
 }
