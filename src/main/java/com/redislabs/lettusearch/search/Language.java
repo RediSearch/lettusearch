06b5e04d86b17ee3dc42f7502ee2180f30114064
Julien Ruaux <jruaux@gmail.com>
2019-04-15 18:52:29 -0700

    introduced language enum

---
+++ src/main/java/com/redislabs/lettusearch/search/Language.java
@@ -0,0 +1,6 @@
+package com.redislabs.lettusearch.search;
+
+public enum Language {
+	Arabic, Danish, Dutch, English, Finnish, French, German, Hungarian, Italian, Norwegian, Portuguese, Romanian,
+	Russian, Spanish, Swedish, Tamil, Turkish, Chinese
+}
