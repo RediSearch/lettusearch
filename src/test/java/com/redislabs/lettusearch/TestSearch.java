8b39d855ec2fa9e9f6dba51e55a44596f30944ca
jruaux <jruaux@gmail.com>
2020-05-18 09:35:10 -0700

    Added unit test for reactive API

--- src/test/java/com/redislabs/lettusearch/TestSearch.java
+++ src/test/java/com/redislabs/lettusearch/TestSearch.java
@@ -22,111 +22,124 @@ import com.redislabs.lettusearch.search.SearchOptions;
 import com.redislabs.lettusearch.search.SearchResults;
 import com.redislabs.lettusearch.search.TagOptions;
 
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class TestSearch extends AbstractBaseTest {
 
-	@Test
-	public void phoneticFields() {
-		SearchResults<String, String> results = commands.search(INDEX, "eldur");
-		assertEquals(7, results.getCount());
-	}
+    @Test
+    public void phoneticFields() {
+        SearchResults<String, String> results = commands.search(INDEX, "eldur");
+        assertEquals(7, results.getCount());
+    }
 
-	@Test
-	public void get() {
-		Map<String, String> map = commands.get(INDEX, "1836");
-		assertEquals("Widmer Brothers Hefeweizen", map.get(FIELD_NAME));
-	}
+    @Test
+    public void get() {
+        Map<String, String> map = commands.get(INDEX, "1836");
+        assertEquals("Widmer Brothers Hefeweizen", map.get(FIELD_NAME));
+    }
 
-	@Test
-	public void mget() throws InterruptedException {
-		List<Map<String, String>> mapList = commands.ftMget(INDEX, "1836", "1837", "292929292");
-		assertEquals(3, mapList.size());
-		assertEquals("Widmer Brothers Hefeweizen", mapList.get(0).get(FIELD_NAME));
-		assertEquals("Hefe Black", mapList.get(1).get(FIELD_NAME));
-		Flux<Map<String, String>> source = connection.reactive().ftMget(INDEX, "1836", "1837");
-		StepVerifier.create(source).expectNextMatches(beer -> beer.get(FIELD_NAME).equals("Widmer Brothers Hefeweizen"))
-				.expectNextMatches(beer -> beer.get(FIELD_NAME).equals("Hefe Black")).expectComplete().verify();
-	}
+    @Test
+    public void mget() throws InterruptedException {
+        List<Map<String, String>> mapList = commands.ftMget(INDEX, "1836", "1837", "292929292");
+        assertEquals(3, mapList.size());
+        assertEquals("Widmer Brothers Hefeweizen", mapList.get(0).get(FIELD_NAME));
+        assertEquals("Hefe Black", mapList.get(1).get(FIELD_NAME));
+        Flux<Map<String, String>> source = connection.reactive().ftMget(INDEX, "1836", "1837");
+        StepVerifier.create(source).expectNextMatches(beer -> beer.get(FIELD_NAME).equals("Widmer Brothers Hefeweizen"))
+                .expectNextMatches(beer -> beer.get(FIELD_NAME).equals("Hefe Black")).expectComplete().verify();
+    }
 
-	@Test
-	public void del() {
-		boolean deleted = commands.del(INDEX, "1836", true);
-		assertTrue(deleted);
-		Map<String, String> map = commands.get(INDEX, "1836");
-		assertNull(map);
-	}
+    @Test
+    public void del() {
+        boolean deleted = commands.del(INDEX, "1836", true);
+        assertTrue(deleted);
+        Map<String, String> map = commands.get(INDEX, "1836");
+        assertNull(map);
+    }
 
-	@Test
-	public void searchNoContent() {
-		SearchResults<String, String> results = commands.search(INDEX, "Hefeweizen", SearchOptions.builder()
-				.withScores(true).noContent(true).limit(Limit.builder().num(100).build()).build());
-		assertEquals(22, results.getCount());
-		assertEquals(22, results.size());
-		assertEquals("1836", results.get(0).getId());
-		assertEquals(1.2, results.get(0).getScore(), 0.000001);
-	}
+    @Test
+    public void searchNoContent() {
+        SearchResults<String, String> results = commands.search(INDEX, "Hefeweizen", SearchOptions.builder()
+                .withScores(true).noContent(true).limit(Limit.builder().num(100).build()).build());
+        assertEquals(22, results.getCount());
+        assertEquals(22, results.size());
+        assertEquals("1836", results.get(0).getId());
+        assertEquals(1.2, results.get(0).getScore(), 0.000001);
+    }
 
-	@Test
-	public void searchWithPayloads() {
-		SearchResults<String, String> results = commands.search(INDEX, "pale",
-				SearchOptions.builder().withPayloads(true).build());
-		assertEquals(256, results.getCount());
-		Document<String, String> result1 = results.get(0);
-		assertNotNull(result1.get(FIELD_NAME));
-		assertNotNull(result1.getPayload());
-		assertEquals(result1.get(FIELD_NAME), result1.getPayload());
-	}
+    @Test
+    public void searchWithPayloads() {
+        SearchResults<String, String> results = commands.search(INDEX, "pale",
+                SearchOptions.builder().withPayloads(true).build());
+        assertEquals(256, results.getCount());
+        Document<String, String> result1 = results.get(0);
+        assertNotNull(result1.get(FIELD_NAME));
+        assertNotNull(result1.getPayload());
+        assertEquals(result1.get(FIELD_NAME), result1.getPayload());
+    }
 
-	@Test
-	public void searchReturn() {
-		SearchResults<String, String> results = commands.search(INDEX, "pale",
-				SearchOptions.builder().returnField(FIELD_NAME).returnField(FIELD_STYLE).build());
-		assertEquals(256, results.getCount());
-		Document<String, String> result1 = results.get(0);
-		assertNotNull(result1.get(FIELD_NAME));
-		assertNotNull(result1.get(FIELD_STYLE));
-		assertNull(result1.get(FIELD_ABV));
-	}
+    @Test
+    public void searchReturn() {
+        SearchResults<String, String> results = commands.search(INDEX, "pale",
+                SearchOptions.builder().returnField(FIELD_NAME).returnField(FIELD_STYLE).build());
+        assertEquals(256, results.getCount());
+        Document<String, String> result1 = results.get(0);
+        assertNotNull(result1.get(FIELD_NAME));
+        assertNotNull(result1.get(FIELD_STYLE));
+        assertNull(result1.get(FIELD_ABV));
+    }
 
-	@Test
-	public void searchInvalidReturn() {
-		SearchResults<String, String> results = commands.search(INDEX, "pale",
-				SearchOptions.builder().returnField(FIELD_NAME).returnField(FIELD_STYLE).returnField("").build());
-		assertEquals(256, results.getCount());
-		Document<String, String> result1 = results.get(0);
-		assertNotNull(result1.get(FIELD_NAME));
-		assertNotNull(result1.get(FIELD_STYLE));
-		assertNull(result1.get(FIELD_ABV));
-	}
+    @Test
+    public void searchInvalidReturn() {
+        SearchResults<String, String> results = commands.search(INDEX, "pale",
+                SearchOptions.builder().returnField(FIELD_NAME).returnField(FIELD_STYLE).returnField("").build());
+        assertEquals(256, results.getCount());
+        Document<String, String> result1 = results.get(0);
+        assertNotNull(result1.get(FIELD_NAME));
+        assertNotNull(result1.get(FIELD_STYLE));
+        assertNull(result1.get(FIELD_ABV));
+    }
 
-	@Test
-	public void searchHighlight() {
-		String term = "pale";
-		String query = "@style:" + term;
-		TagOptions tagOptions = TagOptions.builder().open("<b>").close("</b>").build();
-		SearchResults<String, String> results = commands.search(INDEX, query,
-				SearchOptions.builder().highlight(HighlightOptions.builder().build()).build());
-		for (Document<String, String> result : results) {
-			assertTrue(highlighted(result, FIELD_STYLE, tagOptions, term));
-		}
-		results = commands.search(INDEX, query,
-				SearchOptions.builder().highlight(HighlightOptions.builder().field(FIELD_NAME).build()).build());
-		for (Document<String, String> result : results) {
-			assertFalse(highlighted(result, FIELD_STYLE, tagOptions, term));
-		}
-		tagOptions = TagOptions.builder().open("[start]").close("[end]").build();
-		results = commands.search(INDEX, query, SearchOptions.builder()
-				.highlight(HighlightOptions.builder().field(FIELD_STYLE).tags(tagOptions).build()).build());
-		for (Document<String, String> result : results) {
-			assertTrue(highlighted(result, FIELD_STYLE, tagOptions, term));
-		}
-	}
+    @Test
+    public void searchHighlight() {
+        String term = "pale";
+        String query = "@style:" + term;
+        TagOptions tagOptions = TagOptions.builder().open("<b>").close("</b>").build();
+        SearchResults<String, String> results = commands.search(INDEX, query,
+                SearchOptions.builder().highlight(HighlightOptions.builder().build()).build());
+        for (Document<String, String> result : results) {
+            assertTrue(highlighted(result, FIELD_STYLE, tagOptions, term));
+        }
+        results = commands.search(INDEX, query,
+                SearchOptions.builder().highlight(HighlightOptions.builder().field(FIELD_NAME).build()).build());
+        for (Document<String, String> result : results) {
+            assertFalse(highlighted(result, FIELD_STYLE, tagOptions, term));
+        }
+        tagOptions = TagOptions.builder().open("[start]").close("[end]").build();
+        results = commands.search(INDEX, query, SearchOptions.builder()
+                .highlight(HighlightOptions.builder().field(FIELD_STYLE).tags(tagOptions).build()).build());
+        for (Document<String, String> result : results) {
+            assertTrue(highlighted(result, FIELD_STYLE, tagOptions, term));
+        }
+    }
 
-	private boolean highlighted(Document<String, String> result, String fieldName, TagOptions tags, String string) {
-		String fieldValue = result.get(fieldName).toLowerCase();
-		return fieldValue.contains(tags.getOpen() + string + tags.getClose());
-	}
+    private boolean highlighted(Document<String, String> result, String fieldName, TagOptions tags, String string) {
+        String fieldValue = result.get(fieldName).toLowerCase();
+        return fieldValue.contains(tags.getOpen() + string + tags.getClose());
+    }
+
+    @Test
+    public void reactiveSearch() {
+        SearchResults<String, String> results = connection.reactive().search(INDEX, "pale",
+                SearchOptions.builder().limit(Limit.builder().num(100).offset(200).build()).build()).block();
+        assertEquals(256, results.getCount());
+        Document<String, String> result1 = results.get(0);
+        assertNotNull(result1.get(FIELD_NAME));
+        assertNotNull(result1.get(FIELD_STYLE));
+        assertNotNull(result1.get(FIELD_ABV));
+    }
 
 }
--- src/test/java/com/redislabs/lettusearch/UsageExample.java
+++ src/test/java/com/redislabs/lettusearch/UsageExample.java
@@ -1,35 +1,25 @@
 package com.redislabs.lettusearch;
 
-import static com.redislabs.lettusearch.Beers.FIELD_ABV;
-import static com.redislabs.lettusearch.Beers.FIELD_NAME;
-import static com.redislabs.lettusearch.Beers.FIELD_STYLE;
-import static com.redislabs.lettusearch.Beers.INDEX;
-import static com.redislabs.lettusearch.Beers.load;
-
 import com.redislabs.lettusearch.index.CreateOptions;
 import com.redislabs.lettusearch.index.Schema;
 import com.redislabs.lettusearch.index.field.NumericField;
 import com.redislabs.lettusearch.index.field.PhoneticMatcher;
 import com.redislabs.lettusearch.index.field.TagField;
 import com.redislabs.lettusearch.index.field.TextField;
-import com.redislabs.lettusearch.search.Document;
 import com.redislabs.lettusearch.search.SearchResults;
 
-import java.util.Map;
+import static com.redislabs.lettusearch.Beers.*;
 
 public class UsageExample {
 
-	public final static Schema SCHEMA = Schema.builder()
-			.field(TextField.builder().name(FIELD_NAME).matcher(PhoneticMatcher.English).build())
-			.field(TagField.builder().name(FIELD_STYLE).sortable(true).build())
-			.field(NumericField.builder().name(FIELD_ABV).sortable(true).build()).build();
+    public final static Schema SCHEMA = Schema.builder().field(TextField.builder().name(FIELD_NAME).matcher(PhoneticMatcher.English).build()).field(TagField.builder().name(FIELD_STYLE).sortable(true).build()).field(NumericField.builder().name(FIELD_ABV).sortable(true).build()).build();
 
-	public static void main(String[] args) throws Exception {
-		StatefulRediSearchConnection<String, String> conn = RediSearchClient.create("redis://localhost").connect();
-		RediSearchCommands<String, String> commands = conn.sync();
-		commands.create(Beers.INDEX, SCHEMA, CreateOptions.builder().build());
-		load().forEach(d -> commands.add(Beers.INDEX, d, null));
-		SearchResults<String, String> results = commands.search(INDEX, "chouf*");
-		results.forEach(System.out::println);
-	}
+    public static void main(String[] args) throws Exception {
+        StatefulRediSearchConnection<String, String> conn = RediSearchClient.create("redis://localhost").connect();
+        RediSearchCommands<String, String> commands = conn.sync();
+        commands.create(Beers.INDEX, SCHEMA, CreateOptions.builder().build());
+        load().forEach(d -> commands.add(Beers.INDEX, d, null));
+        SearchResults<String, String> results = commands.search(INDEX, "chouf*");
+        results.forEach(System.out::println);
+    }
 }
