fe6df57f9b16dd5065e5cbfb218ebbb6789d8fd7
jruaux <jruaux@gmail.com>
2021-02-18 21:21:32 -0800

    added unit test for sugget without options

--- src/test/java/com/redislabs/lettusearch/TestSuggest.java
+++ src/test/java/com/redislabs/lettusearch/TestSuggest.java
@@ -15,6 +15,13 @@ public class TestSuggest extends AbstractBaseTest {
     @Test
     public void testSugget() throws IOException {
         createBeerSuggestions();
+        List<Suggestion<String>> results = sync.sugget(SUGINDEX, "Ame");
+        assertEquals(5, results.size());
+    }
+
+    @Test
+    public void testSuggetOptions() throws IOException {
+        createBeerSuggestions();
         List<Suggestion<String>> results = sync.sugget(SUGINDEX, "Ame", SuggetOptions.builder().max(1000L).build());
         assertEquals(8, results.size());
     }
